6015294aa30180e7efe845b5d73d23c7a6e92613
rodgersea <elliottarodgers@gmail.com>
2020-02-20 14:39:01 -0500

    flip and translate

--- javascript.js
+++ javascript.js
@@ -15,4 +15,13 @@ button4.onclick = function() {
     document.getElementById("box").style.height = "150px";
     document.getElementById("box").style.width = "150px";
     document.getElementById("box").style.backgroundColor = "orange";
+    document.getElementById("box").style.opacity = "1";
+}
+
+button5.onclick = function() {
+    document.getElementById("box").style.height = "75px";
+}
+
+button6.onclick = function() {
+    document.getElementById("box").style.transform = "translateX(150px)"
 }
