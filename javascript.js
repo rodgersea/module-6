0e45125b706db6cbe9705f704c98d9e60c42a236
rodgersea <elliottarodgers@gmail.com>
2020-02-20 14:04:01 -0500

    added js

---
+++ javascript.js
@@ -0,0 +1,22 @@
+button1.onclick = function() {
+    document.getElementById("button1").style.height = "200px";
+    document.getElementById("button1").style.width = "200px";
+}
+
+button2.onclick = function() {
+    document.getElementById("button2").style.color = "blue";
+}
+
+button3.onclick = function() {
+    document.getElementById("button3").style.opacity = 1;
+}
+
+button4.onclick = function() {
+    document.getElementById("button4").style.height = "150px";
+    document.getElementById("button4").style.width = "150px";
+    document.getElementById("button2").style.color = "orange";
+}
+
+button5.onlick = function() {
+    document.getElementById("button5").style.height = 
+}
